dcc3d5a43239863c6e30b6af2e09146d1e948a72
Thomas Hvid Spangsberg <thsp@eaaa.dk>
2025-09-01 15:53:16 +0200

    pre-teaching

---
+++ modul-02/DOM-manipulation/Scripts/jsonLoadScript.js
@@ -0,0 +1,36 @@
+let mainContentArea = document.getElementById("main-content-area");
+
+fetchData("Data/data.json").then((data) => {
+  console.log(data);
+
+  for (let i = 0; i < data.length; i++) {
+    let contentToInsert = "";
+
+    let articleToInsert =
+      "<div id='" +
+      data[i].id +
+      "' class='content'>" +
+      "<h1>" +
+      data[i].heading +
+      "</h1>" +
+      "<p class='inner-content'>" +
+      contentToInsert +
+      "</p>" +
+      "<p class='author'>" +
+      data[i].author +
+      "</p>" +
+      "<p class='contact'>" +
+      data[i].email +
+      "</p>" +
+      "</div>";
+
+    mainContentArea.innerHTML = mainContentArea.innerHTML + articleToInsert;
+  }
+});
+
+//Magi - det taler vi om senere!!
+async function fetchData(url) {
+  let request = await fetch(url);
+  let json = await request.json();
+  return json;
+}
