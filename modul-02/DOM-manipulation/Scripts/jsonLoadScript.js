f582798c19b6177d5020137c04d9f2c10fff24c9
Thomas Hvid Spangsberg <thsp@eaaa.dk>
2025-09-10 12:49:35 +0200

    Post-teaching

--- modul-02/DOM-manipulation/Scripts/jsonLoadScript.js
+++ modul-02/DOM-manipulation/Scripts/jsonLoadScript.js
@@ -1,7 +1,34 @@
 let mainContentArea = document.getElementById("main-content-area");
 
 fetchData("Data/data.json").then((data) => {
+  //Al kode som bruger "data" inde mellem de krøllede parateser: {}
+
   console.log(data);
+
+  let articleToInsert = "";
+
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].heading);
+    articleToInsert =
+      "<div id='" +
+      data[i].id +
+      "' class='content'>" +
+      "<h1>" +
+      data[i].heading +
+      "</h1>" +
+      "<p class='inner-content'>" +
+      data[i].content +
+      "</p>" +
+      "<p class='author'>" +
+      data[i].author +
+      "</p>" +
+      "<p class='contact'>" +
+      data[i].email +
+      "</p>" +
+      "</div>";
+
+    mainContentArea.innerHTML += articleToInsert;
+  }
 });
 
 //Magi - det taler vi om senere!!
